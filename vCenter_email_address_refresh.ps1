72749ae5db2498b95a2e494971483e965dd984f0
Eddie <53376139+big-edd@users.noreply.github.com>
2021-09-01 14:07:35 +0800

    update vCenter email refresh email subject and body

--- vCenter_email_address_refresh.ps1
+++ vCenter_email_address_refresh.ps1
@@ -23,6 +23,8 @@ Set-PowerCLIConfiguration -ParticipateInCEIP $false -DisplayDeprecationWarnings 
 ##  Prevent prompt to Update PowerCLI configuration. 
 Set-PowerCLIConfiguration -InvalidCertificateAction Ignore -DisplayDeprecationWarnings $false -Scope Session -Confirm:$false | Out-Null
 
+##  ----------------------------------------------------------------------------
+
 foreach ($vSphereHost in $viServers)
 {
     Write-Host Connecting to $vSphereHost ... 
@@ -30,8 +32,8 @@ foreach ($vSphereHost in $viServers)
 
     Write-Host ------------------------------------------------------------
     Write-Host Checking current email alarms for $vSphereHost ... 
-    Get-AlarmDefinition | Get-AlarmAction -ActionType SendEmail | Select-Object AlarmDefinition,AlarmVersion,Cc,To | Format-Table -AutoSize
-
+    Get-AlarmDefinition | Get-AlarmAction -ActionType SendEmail | Select-Object AlarmDefinition,AlarmVersion,Cc,To,Subject,Body | Format-Table -AutoSize
+    
     Write-Host ------------------------------
     Write-Host Clearing all email alarms  for $vSphereHost ... 
     Get-AlarmDefinition | Get-AlarmAction -ActionType SendEmail | Remove-AlarmAction -Confirm:$false
@@ -40,12 +42,14 @@ foreach ($vSphereHost in $viServers)
     Write-Host Setting required email actions for $vSphereHost ... 
     foreach ($alarmName in $alarmsForEmailAlert)
     {
-        Get-AlarmDefinition -Name $alarmName | New-AlarmAction -Email -To $emailToAddress -Body 'Alarm for $($vSphereHost) regarding $($alarmName).' -Subject '$($vSphereHost) - $($alarmName)' -Confirm:$false | Out-Null
+        $emailSubject = $($vSphereHost) + ' - ' + $($alarmName) + '.'
+        $emailBody = 'Alarm for ' + $($vSphereHost) + ' regarding ' + $($alarmName) + '.'
+        Get-AlarmDefinition -Name $alarmName | New-AlarmAction -Email -To $emailToAddress -Subject $emailSubject -Body $emailBody -Confirm:$false | Out-Null
     }
 
     Write-Host ------------------------------
     Write-Host Confirming new email alarms for $vSphereHost ... 
-    Get-AlarmDefinition | Get-AlarmAction -ActionType SendEmail | Select-Object AlarmDefinition,AlarmVersion,Cc,To | Format-Table -AutoSize
+    Get-AlarmDefinition | Get-AlarmAction -ActionType SendEmail | Select-Object AlarmDefinition,AlarmVersion,Cc,To,Subject,Body | Format-Table -AutoSize
 
     Write-Host ------------------------------------------------------------
     Disconnect-VIServer $vSphereHost -Confirm:$false
